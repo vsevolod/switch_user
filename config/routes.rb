f02ae983e29cc7b50c8ec7f8231496e701f00893
Itay Klein <itay@crossrider.com>
2013-07-21 13:06:01 +0300

    added switch back route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get :switch_user, :to => 'switch_user#set_current_user'
   get 'switch_user/remember_user', :to => 'switch_user#remember_user'
+  get 'switch_user/switch_back', :to => 'switch_user#switch_back'
 end
